5df4499592dda3992a1423626a33269f52d7e35f
yurchykyur <yurchyk.yur.s@gmail.com>
2023-03-19 16:53:57 +0200

    change menu, modal menu,

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,18 +1,3 @@
-// (() => {
-//   const refs = {
-//     openMenuBtn: document.querySelector('[data-menu-open]'),
-//     closeMenuBtn: document.querySelector('[data-menu-close]'),
-//     menu: document.querySelector('[data-menu]'),
-//   };
-
-//   refs.openMenuBtn.addEventListener('click', toggleModal);
-//   refs.closeMenuBtn.addEventListener('click', toggleModal);
-
-//   function toggleModal() {
-//     refs.menu.classList.toggle('is-open');
-//   }
-// })();
-
 (() => {
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
